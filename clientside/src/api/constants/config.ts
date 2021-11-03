dd07b5fdb83ab9e504e73917c0341c727e121eda
GreenColoredLife <greencoloredlife@gmail.com>
2021-11-04 02:31:28 +0700

    meta/fix: Fix env var for cdn

--- clientside/src/api/constants/config.ts
+++ clientside/src/api/constants/config.ts
@@ -1,7 +1,7 @@
 export const API_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8000"
-        : process.env.API_URL;
+        : process.env.REACT_APP_API_URL;
 
 export enum SocketEvent {
     CreateRoom = "createRoom",
