099267bb54c6a306e237b22b321877df057bc05c
winnaries <w.winnaries@gmail.com>
2021-11-05 10:04:11 +0700

    feat: rotate for ipad

--- clientside/src/api/constants/config.ts
+++ clientside/src/api/constants/config.ts
@@ -1,6 +1,6 @@
 export const API_URL =
     process.env.NODE_ENV === "development"
-        ? "http://localhost:8000"
+        ? "https://game.motorhub-th.com"
         : process.env.REACT_APP_API_URL;
 
 export enum SocketEvent {
--- clientside/src/features/setup/index.tsx
+++ clientside/src/features/setup/index.tsx
@@ -1,8 +1,8 @@
-import { FC, MouseEvent, useState, useEffect, useMemo } from "react";
+import { FC, MouseEvent, useState, useEffect, useMemo, useCallback } from "react";
 import { Position, Side } from "../board/types/utility";
 import { PlacementList, PlacementStatus } from "./types/placement";
 import { INIT_PLACEMENT_LIST } from "./constants/list";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Board from "../board";
 import Ship from "./components/ship";
 import Refresh from "./components/refresh";
@@ -17,6 +17,7 @@ import {
     BattleshipStatus,
 } from "../board/types/battleship";
 import getAvailableSquares from "../board/functions/getAvailableSquares";
+import Left from "../game/components/left";
 
 interface Props {
     onSubmit: (shipyard: BattleshipAllyYard) => void;
@@ -161,34 +162,38 @@ const SetupModal: FC<Props> = ({ onSubmit }) => {
         />
     ));
 
+    const onRotate = useCallback(() => {
+        const index = placements.findIndex((p) => p.selected);
+        setDirection((direction + 1) % 4);
+
+        if (index === -1) return;
+
+        const nextPlacements: PlacementList = placements.map((p, i) => {
+            if (i !== index || p.status === PlacementStatus.Undecided)
+                return p;
+            return {
+                battleship: {
+                    ...p.battleship,
+                    direction: (p.battleship.direction + 1) % 4,
+                },
+                selected: p.selected,
+                status: PlacementStatus.Placing,
+            };
+        });
+
+        setPlacements(nextPlacements);
+    }, [placements, direction]);
+
     useEffect(() => {
         const fn = (e: KeyboardEvent) => {
             if (e.code === "Space") {
-                const index = placements.findIndex((p) => p.selected);
-
-                if (index === -1) return;
-
-                const nextPlacements: PlacementList = placements.map((p, i) => {
-                    if (i !== index || p.status === PlacementStatus.Undecided)
-                        return p;
-                    return {
-                        battleship: {
-                            ...p.battleship,
-                            direction: (p.battleship.direction + 1) % 4,
-                        },
-                        selected: p.selected,
-                        status: PlacementStatus.Placing,
-                    };
-                });
-
-                setDirection((direction + 1) % 4);
-                setPlacements(nextPlacements);
+                onRotate();
             }
         };
 
         document.addEventListener("keypress", fn);
         return () => document.removeEventListener("keypress", fn);
-    }, [placements, direction]);
+    }, [placements, direction, onRotate]);
 
     return (
         <Wrapper>
@@ -216,7 +221,10 @@ const SetupModal: FC<Props> = ({ onSubmit }) => {
                         onSquareHoverEnd={onSquareHoverEnd}
                         onSquareClick={onSquareClick}
                     ></Board>
-                    <ResetButton onClick={onReset}>Reset</ResetButton>
+                    <MoreAction>
+                        <RotateButton direction={direction} onClick={onRotate}><Left size="1.5rem" color="white"/></RotateButton>
+                        <ResetButton onClick={onReset}>Reset</ResetButton>
+                    </MoreAction>
                 </BoardWrapper>
             </BodyWrapper>
             <Spacer />
@@ -257,6 +265,16 @@ const HeaderText = styled.div`
     color: white;
 `;
 
+const MoreAction = styled.div`
+    display: flex; 
+    flex-flow: row; 
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 100%;
+    margin-top: 1rem;
+`;
+
 const RandomButton = styled.button`
     outline: none;
     font-size: 1rem;
@@ -273,6 +291,7 @@ const RandomButton = styled.button`
     color: white;
     cursor: pointer;
     font-weight: 600;
+    width: auto;
 
     &:hover {
         transform: scaleX(1.04) scaleY(1.08);
@@ -282,6 +301,7 @@ const RandomButton = styled.button`
         transform: scaleX(0.96) scaleY(0.92);
     }
 `;
+
 
 const BodyWrapper = styled.div`
     display: flex;
@@ -353,9 +373,8 @@ const ResetButton = styled.button`
     color: white;
     cursor: pointer;
     font-weight: 500;
-    width: 100%;
-    position: absolute;
     bottom: 0;
+    flex-grow: 1; 
 
     &:hover {
         transform: scaleX(1.04) scaleY(1.08);
@@ -363,5 +382,35 @@ const ResetButton = styled.button`
 
     &:active {
         transform: scaleX(0.96) scaleY(0.92);
+    }
+`;
+
+const RotateButton = styled(ResetButton)<{ direction: BattleshipDirection }>`
+    padding: 0.5rem;
+    flex-grow: 0;
+
+    & > svg {
+        transition: transform 200ms ease; 
+
+        ${({ direction }) => {
+            switch (direction) {
+                case BattleshipDirection.Horizontal:
+                    return css`
+                        transform: rotate(0deg);
+                    `;
+                case BattleshipDirection.Vertical:
+                    return css`
+                        transform: rotate(90deg);
+                    `;
+                case BattleshipDirection.HorizontalRev:
+                    return css`
+                        transform: rotate(180deg);
+                    `;
+                case BattleshipDirection.VerticalRev:
+                    return css`
+                        transform: rotate(270deg);
+                    `;
+            }
+        }}
     }
 `;
